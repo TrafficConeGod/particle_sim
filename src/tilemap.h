7b1c33635a1f7e192d7043229f4833fe407acecc
TrafficConeGod <trafficconegod@github.com>
2023-01-05 13:41:38 -0500

    increase tilemap size

--- src/tilemap.h
+++ src/tilemap.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <stdint.h>
 
-#define TILEMAP_WIDTH 160
-#define TILEMAP_HEIGHT 90
+#define TILEMAP_WIDTH 320
+#define TILEMAP_HEIGHT 180
 #define NUM_TILES (TILEMAP_WIDTH * TILEMAP_HEIGHT)
